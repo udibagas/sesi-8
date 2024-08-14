a4286b8c6aaa69eb89029c1b0894fa115ee10c1b
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-14 10:14:47 +0700

    update

---
+++ data/products.sql
@@ -0,0 +1,143 @@
+insert into
+    "Products" (name, stock, price)
+values (
+        'Stock - Chicken, White',
+        31,
+        153000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Sauce Tomato Pouch',
+        23,
+        90000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values ('Filo Dough', 87, 110000);
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Longos - Grilled Chicken With',
+        54,
+        93000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Carbonated Water - Orange',
+        55,
+        157000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Nantucket Apple Juice',
+        16,
+        93000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Truffle Cups - White Paper',
+        61,
+        104000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Wine - Red, Pinot Noir, Chateau',
+        38,
+        186000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values ('Sherry - Dry', 12, 134000);
+
+insert into
+    "Products" (name, stock, price)
+values ('Cornish Hen', 36, 54000);
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Cookies - Oreo, 4 Pack',
+        34,
+        122000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Container Clear 8 Oz',
+        0,
+        177000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values ('Peas Snow', 13, 168000);
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Versatainer Nc - 9388',
+        39,
+        57000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Sausage - Blood Pudding',
+        0,
+        193000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Pasta - Cannelloni, Sheets, Fresh',
+        2,
+        110000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Coffee - Hazelnut Cream',
+        14,
+        102000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Wine - Fume Blanc Fetzer',
+        35,
+        191000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Sugar - Brown, Individual',
+        27,
+        196000
+    );
+
+insert into
+    "Products" (name, stock, price)
+values (
+        'Lid - 16 Oz And 32 Oz',
+        11,
+        116000
+    );
